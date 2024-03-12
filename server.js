42f93ad639dcbe8c6d0a9bdae77bdd5cf7d6aa45
ando <terbu102@gmail.com>
2024-03-12 14:21:52 +0300

    user controller

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import startServer from './libs/boot';
 import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
 const server = express();
 
+injectMiddlewares(server);
 injectRoutes(server);
+startServer(server);
 
 export default server;
